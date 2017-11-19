cff4202b5a4b5ad446e5a5869488d2dae3159145
Hector <hector.riquelme1169@gmail.com>
2017-11-19 20:12:07 -0300

    Parcial componentes de login y registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,21 @@
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './views/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginFormComponent } from './components/login-form/login-form.component';
 
 export const ROUTES = RouterModule.forRoot([
     {
         path: '',
-        component: LoginComponent
+        component: LoginComponent,
+        children: [
+            {
+                path: 'signup',
+                component: RegisterComponent
+            },
+            {
+                path: '',
+                component: LoginFormComponent
+            }
+        ]
     }
 ]);
--- src/app/views/login/login.module.ts
+++ src/app/views/login/login.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
 import { LoginComponent } from './login.component';
+import { HeaderComponent } from '../../components/header/header.component';
+import { RegisterComponent } from '../../components/register/register.component';
+import { LoginFormComponent } from '../../components/login-form/login-form.component';
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
-  declarations: [LoginComponent]
+  declarations: [
+    LoginComponent,
+    HeaderComponent,
+    RegisterComponent,
+    LoginFormComponent
+  ]
 })
 export class LoginModule { }
