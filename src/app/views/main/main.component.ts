b09ef80c6f2c5b473ad6d823fc1efe5ef3eca274
Hector <hector.riquelme1169@gmail.com>
2017-11-23 22:16:31 -0300

    Diseño parcial relaciones de conceptos

--- src/app/views/main/main.component.ts
+++ src/app/views/main/main.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var jQuery;
+
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
@@ -10,6 +12,21 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+  jQuery('ul.tabs').tabs();
+
+  jQuery('.collapsible').collapsible();
+  jQuery('select').material_select();
+  jQuery('.dropdown').dropdown({
+    inDuration: 300,
+    outDuration: 225,
+    constrainWidth: false,
+    hover: false,
+    gutter: 0,
+    belowOrigin: true,
+    alignment: 'left',
+    stopPropagation: false
+    });
   }
 
 }
